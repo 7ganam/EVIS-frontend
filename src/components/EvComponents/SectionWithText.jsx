9bc44009847aaecaa33fee9d2f13cf2d41aec5ad
7ganam@gmail.com <7ganam@gmail.com>
2022-07-28 16:06:23 +0200

    added buttons section

--- src/components/EvComponents/SectionWithText.jsx
+++ src/components/EvComponents/SectionWithText.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import {  Box } from "@mui/material";
-import { Paragraph  } from "components/EvComponents/Typography";
-import {BlockTitle} from "components/EvComponents/StyledTypography";
-import ReadMoreButton from "components/EvComponents/ReadMoreButton";
+import { Box } from "@mui/material";
+import { Paragraph } from "components/EvComponents/Typography";
+import { BlockTitle } from "components/EvComponents/StyledTypography";
+import { ReadMoreButton } from "components/EvComponents/Buttons";
 
 const SectionWithText = (props) => {
-  const { title, text , cmd} = props.data;
-  return <Box sx = {{pt : 2}}>
-    <BlockTitle>{title}</BlockTitle>
-    <Paragraph
-    color = "grey.600"
-    >
-        {text}
-    </Paragraph>
-    <ReadMoreButton cmd = {cmd}/>
-  </Box>;
+  const { title, text, cmd } = props.data;
+  return (
+    <Box sx={{ pt: 2 }}>
+      <BlockTitle>{title}</BlockTitle>
+      <Paragraph color="grey.600">{text}</Paragraph>
+      <ReadMoreButton cmd={cmd} />
+    </Box>
+  );
 };
 
 export default SectionWithText;
