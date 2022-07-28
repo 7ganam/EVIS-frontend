1191020abef5e9f8138e4a7c4b976285585cf1fe
tarekanwer <tito.a7mad@gmail.com>
2022-07-28 15:47:18 +0200

    further edits

--- src/components/EvComponents/SectionWithText.jsx
+++ src/components/EvComponents/SectionWithText.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { useTheme, Box } from "@mui/material";
+import {  Box } from "@mui/material";
 import { Paragraph  } from "components/EvComponents/Typography";
 import {BlockTitle} from "components/EvComponents/StyledTypography";
 import ReadMoreButton from "components/EvComponents/ReadMoreButton";
 
 const SectionWithText = (props) => {
-    const theme = useTheme();
-    const { title, text , cmd} = props.data;
+  const { title, text , cmd} = props.data;
   return <Box sx = {{pt : 2}}>
     <BlockTitle>{title}</BlockTitle>
     <Paragraph
