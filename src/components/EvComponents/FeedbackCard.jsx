539b26a94003eed195f88fc90d3f6c69fe89cd92
tarekanwer <tito.a7mad@gmail.com>
2022-07-29 01:24:24 +0200

    edit

--- src/components/EvComponents/FeedbackCard.jsx
+++ src/components/EvComponents/FeedbackCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Grid, Card, Box } from "@mui/material";
-import { H1, H2, H3 } from "components/EvComponents/Typography";
+import { H4 } from "components/EvComponents/Typography";
 
 const FeedbackCard = () => {
   return (
@@ -9,12 +9,15 @@ const FeedbackCard = () => {
         sx={{
           textAlign : "center",
           backgroundColor: "#f5f5f5",
-          height: "350px",
-            width: "250px",
-          borderRadius: "20%",
+          height: "400px",
+          width: "350px",
+          borderRadius: "30%",
         }}
       >
-        <H3>Feedback</H3>
+        <H4 
+        sx = {{pt : 6 ,px : 6}}
+        >
+            this is th feedback given from certain person and this text is made for testing </H4>
       </Box>
     </Box>
   );
--- src/components/EvSections/FeedbackCardsSection.jsx
+++ src/components/EvSections/FeedbackCardsSection.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import FeedbackCard from 'components/EvComponents/FeedbackCard';
+import {Box} from "@mui/system";
+import { SectionTitle } from 'components/EvComponents/StyledTypography';
 
 const FeedbackCardsSection = () => {
   return (
+    <Box
+    sx = {{
+        mt : 2,
+    }}
+    >
+    <SectionTitle>Feedback Boxes Section</SectionTitle>
     <FeedbackCard/>
+    </Box>
   )
 }
 
