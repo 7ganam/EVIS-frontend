56586ed7f87a1d101312b593e2eaf84010426aeb
Omar ZIdan <s-omarzidan@zewailcity.edu.eg>
2022-07-29 05:01:00 +0200

    removed unnecessary imports

--- src/components/EvComponents/SponsorImage.jsx
+++ src/components/EvComponents/SponsorImage.jsx
@@ -1,7 +1,5 @@
 import { Box } from "@mui/system";
 import React from "react";
-import Image from 'next/image';
-import { Grid } from "@mui/material";
 
 const SponsorImage = (props) => {
     return (
