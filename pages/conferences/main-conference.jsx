3e329243c22c5055563d1ac6a5e208f26a100f15
tarekanwer <tito.a7mad@gmail.com>
2022-08-02 22:09:55 +0200

    init

--- pages/conferences/main-conference.jsx
+++ pages/conferences/main-conference.jsx
@@ -1,17 +1,65 @@
-import { Box, styled, useTheme, Grid, Container } from "@mui/material";
+import {Container } from "@mui/material";
 import EvLayout from "components/layouts/EvLayout";
 import api from "utils/api/grocery3-shop";
-import Landing from "pages-sections/conference-page-sections/Landing";
-import LandingText from "pages-sections/conference-page-sections/LandingText";
+import Landing from "components/EvSections/conference-page-sections/Landing";
+import LandingText from "components/EvSections/conference-page-sections/LandingText";
+import SummitThemes from "components/EvSections/conference-page-sections/SummitThemes";
 
 
 // ======================================================
 // ======================================================
+const section = {
+  main : " The motto for the Summit is “Smart Vehicle Mobility” with four themes (concentrations) that are in perfect synergy with Abu Dhabi, which is witnessing an overwhelming energy greening and sustainability awareness trends. Abu Dhabi has proven to be one of the fastest growing smart cities worldwide adopting the latest green technologies after the successful establishment of Masdar City, the first of its kind worldwide.",
+  t1 : "Paid Conference",
+  t2 : "Open Tech Sessions",
+  p1 : "Each conference day kicks off with a notable keynote address followed by a roundtable panel of top industry leaders discussing the most important market disruptors of the last year and what they expect to see in the years to come.",
+  p2 : "Explore the latest technical content and developments in the industry which includes presentations, panel discussions and case studies, all in the show floor and free to attend for everyone.",
+  btnText : "Register Now",
+}
+
+const themes = [
+  {
+      img: "/assets/images/conference-overview/1.png",
+      title: "NEW ENERGY VEHICLES",
+      p1: "​​​​Passenger Battery Electric Vehicles (BEV)",
+      p2: "Hydrogen Fuel Cell Vehicles (FCEV)",
+      p3 : "Two & Three Wheeler Electric Vehicles",
+      p4 : "Electric Planes",
+      direction: "left",
+  },
+  {
+      img: "/assets/images/conference-overview/2.png",
+      title: "ENERGY & INFRASTRUCTURE",
+      p1: "EV Charging Infrastructure",
+      p2 :"EV Charging Point Management Systems (CPMS)",
+      p3 : "Smart Charging and Cybersecurity",
+      p4 : "Grid Integration",
+      direction: "right",
+
+  },
+  {
+      img: "/assets/images/conference-overview/3.png",
+      title: "POWERTRAIN",
+      p1 : "​​​​Technology Developments",
+      p2 : "Motors and E-Drive Systems",
+      p3: "Battery Management Systems (BMS)",
+      p4 : "",
+      direction: "left",
+  },
+  {
+      img: "/assets/images/conference-overview/4.png",
+      title: "BATTERIES",
+      p1 : "​​​Battery Technology & Development",
+      p2 : "Second Life Batteries",
+      p3 : "Disposal & Recycling",
+      p4 : "Sustainable & Green Batteries",
+      direction: "right",
+  }
+];
 
 
 
-const generalPage = (props) => {
-  const { offerProducts, allProducts, topSailedProducts } = props;
+const generalPage = () => {
 
   return (
     <EvLayout showNavbar={true}>
@@ -21,7 +69,8 @@ const generalPage = (props) => {
           mb: 6,
         }}
       >
-        <LandingText />
+        <LandingText section = {section} />
+        <SummitThemes data = {themes} />
 
 
 
--- src/components/EvComponents/TwoColumnBox.jsx
+++ src/components/EvComponents/TwoColumnBox.jsx
@@ -7,14 +7,14 @@ import Image from "components/BazarImage";
 
 
 
-const TwoColumnContent = (img , title , p1 , p2 , p3 ,direction) => {
+const TwoColumnContent = (img , title , p1 , p2 , p3 , p4 ,direction) => {
     if(direction === "left"){
         return ( <Grid container spacing={5} >
         <Grid item md={6} sm={12} xs={12}>
           <Box sx={{}}>
             <Image
               width={"100%"}
-              height={"100%"}
+              height={"320px"}
               mb={0}
               src={img}
               alt="logo"
@@ -23,17 +23,28 @@ const TwoColumnContent = (img , title , p1 , p2 , p3 ,direction) => {
           </Box>
         </Grid>
         <Grid item md={6} sm={12} xs={12}>
-          <Box sx={{}}>
+        <Box sx={{}}>
             <BlockTitle> {title} </BlockTitle>
+            <Box>
             <Paragraph>
                 {p1}
             </Paragraph>
-            <Paragraph sx = {{pt : 2}}>
+            </Box>
+            <Box sx = {{pt : 2}}>
+            <Paragraph >
                 {p2}
             </Paragraph>
-            <Paragraph sx = {{pt : 2}}>
+            </Box>
+            <Box sx = {{pt : 2}}>
+            <Paragraph>
                 {p3}
             </Paragraph>
+            </Box>
+            <Box sx = {{pt : 2}}>
+            <Paragraph>
+                {p4}
+            </Paragraph>
+            </Box>
           </Box>
         </Grid>
       </Grid>)
@@ -59,13 +70,18 @@ const TwoColumnContent = (img , title , p1 , p2 , p3 ,direction) => {
                 {p3}
             </Paragraph>
             </Box>
+            <Box sx = {{pt : 2}}>
+            <Paragraph>
+                {p4}
+            </Paragraph>
+            </Box>
           </Box>
         </Grid>
         <Grid item md={6} sm={12} xs={12} order = {{xs : 1 , md : 2}}>
           <Box sx={{}}>
             <Image
               width={"100%"}
-              height={"100%"}
+              height={"320px"}
               mb={0}
               src={img}
               alt="logo"
@@ -81,10 +97,10 @@ const TwoColumnContent = (img , title , p1 , p2 , p3 ,direction) => {
 
 
 const TwoColumnBox = (props) => {
-  const { img , title , p1 , p2 , p3 , direction } = props.item;
+  const { img , title , p1 , p2 , p3 , p4 , direction } = props.item;
   return (
     <Box sx={{ pt: "50px", px: 6 }}>
-        {TwoColumnContent(img , title , p1 , p2 , p3  , direction)}
+        {TwoColumnContent(img , title , p1 , p2 , p3 ,p4  , direction)}
     </Box>
   );
 };
