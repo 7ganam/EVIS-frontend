5b7f7c67b0c4ba3fd89baf0c58680b04ac12145a
tarekanwer <tito.a7mad@gmail.com>
2022-08-03 20:28:38 +0200

    format

--- src/components/EvSections/about-page-sections/LandingText.jsx
+++ src/components/EvSections/about-page-sections/LandingText.jsx
@@ -1,34 +1,30 @@
 import React from "react";
 import { Paragraph } from "components/EvComponents/Typography";
-import { Box , Grid } from "@mui/material";
-import {  SectionTitle } from "components/EvComponents/StyledTypography";
-
-
+import { Box, Grid } from "@mui/material";
+import { SectionTitle } from "components/EvComponents/StyledTypography";
 
 const LandingText = (props) => {
-  const  {p1 , p2 , p3 }  = props.section;
+  const { p1, p2, p3 } = props.section;
   return (
-    
-    <Box sx={{ mt: "10px" , py :5 , px : 6  }}>
-        <SectionTitle>EVENT OVERVIEW</SectionTitle>
-     <Grid container spacing={3} sx = {{pt : 5}}>
-        <Grid item xs={12} sm = {12} md = {12}>
-            <Box >
-                <Paragraph>{p1}</Paragraph>
-            </Box>
+    <Box sx={{ mt: "10px", py: 5, px: 6 }}>
+      <SectionTitle>EVENT OVERVIEW</SectionTitle>
+      <Grid container spacing={3} sx={{ pt: 5 }}>
+        <Grid item xs={12} sm={12} md={12}>
+          <Box>
+            <Paragraph>{p1}</Paragraph>
+          </Box>
         </Grid>
-        <Grid item xs={12} sm = {12} md = {12}>
-            <Box >
-                <Paragraph>{p2}</Paragraph>
-            </Box>
+        <Grid item xs={12} sm={12} md={12}>
+          <Box>
+            <Paragraph>{p2}</Paragraph>
+          </Box>
         </Grid>
-        <Grid item xs={12} sm = {12} md = {12}>
-            <Box >
-                <Paragraph>{p3}</Paragraph>
-            </Box>
+        <Grid item xs={12} sm={12} md={12}>
+          <Box>
+            <Paragraph>{p3}</Paragraph>
+          </Box>
         </Grid>
-
-    </Grid>
+      </Grid>
     </Box>
   );
 };
