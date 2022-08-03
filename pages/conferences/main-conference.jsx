e5f5c770e94f8e8215c0e93bfa9a3862050828df
7ganam@gmail.com <7ganam@gmail.com>
2022-08-03 19:37:02 +0200

    fixed some issues

--- pages/conferences/main-conference.jsx
+++ pages/conferences/main-conference.jsx
@@ -1,4 +1,4 @@
-import {Container } from "@mui/material";
+import { Container } from "@mui/material";
 import EvLayout from "components/layouts/EvLayout";
 import api from "utils/api/grocery3-shop";
 import Landing from "components/EvSections/conference-page-sections/Landing";
@@ -8,130 +8,133 @@ import Speakers from "components/EvSections/conference-page-sections/Speakers";
 import Table from "components/EvSections/conference-page-sections/Table";
 import Footer from "components/EvSections/why-exhibit-sections/Footer";
 
-
 // ======================================================
 // ======================================================
 
 const footer1 = [
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQyODYxZjIzZDk0NjRmNjA=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQyODYxZjIzZDk0NjRmNjA=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ0ODYxZjIzZGE4N2NhZWM=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ0ODYxZjIzZGE4N2NhZWM=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ3MzYxZjIzZGMxMDU2Mjc=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ3MzYxZjIzZGMxMDU2Mjc=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ4NTYxZjIzZGNkZTU0ODc=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ4NTYxZjIzZGNkZTU0ODc=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Njk4NTkyODYyMmIwMmM4OTk4YjE=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Njk4NTkyODYyMmIwMmM4OTk4YjE=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Njk4NTk2MTYyMmIwMmU5OGQ3MjA=.png",
-  }
-]
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Njk4NTk2MTYyMmIwMmU5OGQ3MjA=.png",
+  },
+];
 
 const footer2 = [
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjE0MjYxZmQyZDNlNGE3OWY=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjE0MjYxZmQyZDNlNGE3OWY=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjEzMjYxZmQyZDM0NWQ4MzE=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjEzMjYxZmQyZDM0NWQ4MzE=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjExMDYxZmQyZDFlNGVlMjY=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjExMDYxZmQyZDFlNGVlMjY=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjEyMzYxZmQyZDJiMGY1NTE=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjEyMzYxZmQyZDJiMGY1NTE=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0NjAyNjk5NTYyMWM2MGYzZTVhODE=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0NjAyNjk5NTYyMWM2MGYzZTVhODE=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0NjAyNjk4NDYyMWM2MGU4ZGVhYjI=.png",
-  }
-]
-
-
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0NjAyNjk4NDYyMWM2MGU4ZGVhYjI=.png",
+  },
+];
 
 const section = {
-  main : " The motto for the Summit is “Smart Vehicle Mobility” with four themes (concentrations) that are in perfect synergy with Abu Dhabi, which is witnessing an overwhelming energy greening and sustainability awareness trends. Abu Dhabi has proven to be one of the fastest growing smart cities worldwide adopting the latest green technologies after the successful establishment of Masdar City, the first of its kind worldwide.",
-  t1 : "Paid Conference",
-  t2 : "Open Tech Sessions",
-  p1 : "Each conference day kicks off with a notable keynote address followed by a roundtable panel of top industry leaders discussing the most important market disruptors of the last year and what they expect to see in the years to come.",
-  p2 : "Explore the latest technical content and developments in the industry which includes presentations, panel discussions and case studies, all in the show floor and free to attend for everyone.",
-  btnText : "Register Now",
-}
+  main: " The motto for the Summit is “Smart Vehicle Mobility” with four themes (concentrations) that are in perfect synergy with Abu Dhabi, which is witnessing an overwhelming energy greening and sustainability awareness trends. Abu Dhabi has proven to be one of the fastest growing smart cities worldwide adopting the latest green technologies after the successful establishment of Masdar City, the first of its kind worldwide.",
+  t1: "Paid Conference",
+  t2: "Open Tech Sessions",
+  p1: "Each conference day kicks off with a notable keynote address followed by a roundtable panel of top industry leaders discussing the most important market disruptors of the last year and what they expect to see in the years to come.",
+  p2: "Explore the latest technical content and developments in the industry which includes presentations, panel discussions and case studies, all in the show floor and free to attend for everyone.",
+  btnText: "Register Now",
+};
 
 const themes = [
   {
-      img: "/assets/images/conference-overview/1.png",
-      title: "NEW ENERGY VEHICLES",
-      p1: "​​​​Passenger Battery Electric Vehicles (BEV)",
-      p2: "Hydrogen Fuel Cell Vehicles (FCEV)",
-      p3 : "Two & Three Wheeler Electric Vehicles",
-      p4 : "Electric Planes",
-      direction: "left",
+    img: "/assets/images/conference-overview/1.png",
+    title: "NEW ENERGY VEHICLES",
+    p1: "​​​​Passenger Battery Electric Vehicles (BEV)",
+    p2: "Hydrogen Fuel Cell Vehicles (FCEV)",
+    p3: "Two & Three Wheeler Electric Vehicles",
+    p4: "Electric Planes",
+    direction: "left",
   },
   {
-      img: "/assets/images/conference-overview/2.png",
-      title: "ENERGY & INFRASTRUCTURE",
-      p1: "EV Charging Infrastructure",
-      p2 :"EV Charging Point Management Systems (CPMS)",
-      p3 : "Smart Charging and Cybersecurity",
-      p4 : "Grid Integration",
-      direction: "right",
-
+    img: "/assets/images/conference-overview/2.png",
+    title: "ENERGY & INFRASTRUCTURE",
+    p1: "EV Charging Infrastructure",
+    p2: "EV Charging Point Management Systems (CPMS)",
+    p3: "Smart Charging and Cybersecurity",
+    p4: "Grid Integration",
+    direction: "right",
   },
   {
-      img: "/assets/images/conference-overview/3.png",
-      title: "POWERTRAIN",
-      p1 : "​​​​Technology Developments",
-      p2 : "Motors and E-Drive Systems",
-      p3: "Battery Management Systems (BMS)",
-      p4 : "",
-      direction: "left",
+    img: "/assets/images/conference-overview/3.png",
+    title: "POWERTRAIN",
+    p1: "​​​​Technology Developments",
+    p2: "Motors and E-Drive Systems",
+    p3: "Battery Management Systems (BMS)",
+    p4: "",
+    direction: "left",
   },
   {
-      img: "/assets/images/conference-overview/4.png",
-      title: "BATTERIES",
-      p1 : "​​​Battery Technology & Development",
-      p2 : "Second Life Batteries",
-      p3 : "Disposal & Recycling",
-      p4 : "Sustainable & Green Batteries",
-      direction: "right",
-  }
+    img: "/assets/images/conference-overview/4.png",
+    title: "BATTERIES",
+    p1: "​​​Battery Technology & Development",
+    p2: "Second Life Batteries",
+    p3: "Disposal & Recycling",
+    p4: "Sustainable & Green Batteries",
+    direction: "right",
+  },
 ];
-
-
-
 
 const imagesData = [
   {
-      imageSrc: "/assets/images/speakers/1.png",
-      name: "Alex Gilbert",
-      title: "Head of Energy & Electrification - Commercial Development Transport for London (TfL)"
+    imageSrc: "/assets/images/speakers/1.png",
+    name: "Alex Gilbert",
+    title:
+      "Head of Energy & Electrification - Commercial Development Transport for London (TfL)",
   },
   {
-      imageSrc: "/assets/images/speakers/2.png",
-      name: "Claas Bracklo",
-      title: "Head of Electromobility at BMW Group & Chairman at CharIN"
+    imageSrc: "/assets/images/speakers/2.png",
+    name: "Claas Bracklo",
+    title: "Head of Electromobility at BMW Group & Chairman at CharIN",
   },
   {
-      imageSrc: "/assets/images/speakers/3.png",
-      name: "Dr. Nasser Saidi",
-      title: "Chairman Clean Energy Business Council (CEBC)"
-  }
+    imageSrc: "/assets/images/speakers/3.png",
+    name: "Dr. Nasser Saidi",
+    title: "Chairman Clean Energy Business Council (CEBC)",
+  },
 ];
-
 
 const btnTxt = "See All Speakers";
 
-
 const generalPage = () => {
-
   return (
     <EvLayout showNavbar={true}>
       <Landing />
@@ -139,18 +142,13 @@ const generalPage = () => {
         sx={{
           mb: 6,
         }}
+        maxWidth="lg"
       >
-        <LandingText section = {section} />
-        <SummitThemes data = {themes} />
-        <Speakers data = {imagesData} btn = {btnTxt} />
+        <LandingText section={section} />
+        <SummitThemes data={themes} />
+        <Speakers data={imagesData} btn={btnTxt} />
         <Table />
-        <Footer footer1 = {footer1} footer2 = {footer2}/>
-
-
-
-
-
-
+        <Footer footer1={footer1} footer2={footer2} />
       </Container>
     </EvLayout>
   );
--- src/components/EvComponents/TwoColumnBox.jsx
+++ src/components/EvComponents/TwoColumnBox.jsx
@@ -4,103 +4,84 @@ import { BlockTitle } from "components/EvComponents/StyledTypography";
 import { Paragraph } from "components/EvComponents/Typography";
 import Image from "components/BazarImage";
 
-
-
-
-const TwoColumnContent = (img , title , p1 , p2 , p3 , p4 ,direction) => {
-    if(direction === "left"){
-        return ( <Grid container spacing={5} >
+const TwoColumnContent = (img, title, p1, p2, p3, p4, direction) => {
+  if (direction === "left") {
+    return (
+      <Grid container spacing={2} sx={{ p: 0 }} justifyContent="flex-start">
         <Grid item md={6} sm={12} xs={12}>
           <Box sx={{}}>
             <Image
               width={"100%"}
-              height={"320px"}
+              // height={"320px"}
               mb={0}
               src={img}
               alt="logo"
-              sx={{ objectFit: "cover" }}
+              sx={{ objectFit: "cover", aspectRatio: " 16 / 10" }}
             />
           </Box>
         </Grid>
         <Grid item md={6} sm={12} xs={12}>
-        <Box sx={{}}>
+          <Box sx={{ pl: { md: "20px" } }}>
             <BlockTitle> {title} </BlockTitle>
-            <Box>
-            <Paragraph>
-                {p1}
-            </Paragraph>
+            <Box sx={{ mt: "10px" }}>
+              <Paragraph>{p1}</Paragraph>
             </Box>
-            <Box sx = {{pt : 2}}>
-            <Paragraph >
-                {p2}
-            </Paragraph>
+            <Box sx={{ pt: 2 }}>
+              <Paragraph>{p2}</Paragraph>
             </Box>
-            <Box sx = {{pt : 2}}>
-            <Paragraph>
-                {p3}
-            </Paragraph>
+            <Box sx={{ pt: 2 }}>
+              <Paragraph>{p3}</Paragraph>
             </Box>
-            <Box sx = {{pt : 2}}>
-            <Paragraph>
-                {p4}
-            </Paragraph>
+            <Box sx={{ pt: 2 }}>
+              <Paragraph>{p4}</Paragraph>
             </Box>
           </Box>
         </Grid>
-      </Grid>)
-
-    } 
-    if (direction === "right"){
-        return( <Grid container spacing={5}>
-        <Grid item md={6} sm={12} xs={12} order = {{xs : 2 , md : 1}}>
-        <Box sx={{}}>
-            <BlockTitle> {title} </BlockTitle>
-            <Box>
-            <Paragraph>
-                {p1}
-            </Paragraph>
-            </Box>
-            <Box sx = {{pt : 2}}>
-            <Paragraph >
-                {p2}
-            </Paragraph>
-            </Box>
-            <Box sx = {{pt : 2}}>
-            <Paragraph>
-                {p3}
-            </Paragraph>
-            </Box>
-            <Box sx = {{pt : 2}}>
-            <Paragraph>
-                {p4}
-            </Paragraph>
-            </Box>
-          </Box>
-        </Grid>
-        <Grid item md={6} sm={12} xs={12} order = {{xs : 1 , md : 2}}>
+      </Grid>
+    );
+  }
+  if (direction === "right") {
+    return (
+      <Grid container spacing={2}>
+        <Grid item md={6} sm={12} xs={12} order={{ xs: 2, md: 1 }}>
           <Box sx={{}}>
+            <BlockTitle> {title} </BlockTitle>
+            <Box sx={{ mt: "10px" }}>
+              <Paragraph>{p1}</Paragraph>
+            </Box>
+            <Box sx={{ pt: 2 }}>
+              <Paragraph>{p2}</Paragraph>
+            </Box>
+            <Box sx={{ pt: 2 }}>
+              <Paragraph>{p3}</Paragraph>
+            </Box>
+            <Box sx={{ pt: 2 }}>
+              <Paragraph>{p4}</Paragraph>
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item md={6} sm={12} xs={12} order={{ xs: 1, md: 2 }}>
+          <Box sx={{ pl: { md: "20px" } }}>
             <Image
               width={"100%"}
-              height={"320px"}
+              // height={"320px"}
               mb={0}
               src={img}
               alt="logo"
-              sx={{ objectFit: "cover" }}
+              sx={{ objectFit: "cover", aspectRatio: " 16 / 10" }}
             />
           </Box>
         </Grid>
-      </Grid>)
-    }
-
-
-}
-
+      </Grid>
+    );
+  }
+};
 
 const TwoColumnBox = (props) => {
-  const { img , title , p1 , p2 , p3 , p4 , direction } = props.item;
+  const { img, title, p1, p2, p3, p4, direction } = props.item;
   return (
     <Box sx={{ pt: "50px", px: 6 }}>
-        {TwoColumnContent(img , title , p1 , p2 , p3 ,p4  , direction)}
+      {TwoColumnContent(img, title, p1, p2, p3, p4, direction)}
     </Box>
   );
 };
