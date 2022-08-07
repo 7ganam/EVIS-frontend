f3a84df3364a05ca84926fd36fe938f73d8d518e
7ganam@gmail.com <7ganam@gmail.com>
2022-08-07 14:35:49 +0200

    fixed why exhibit paragraph

--- pages/exhibition/why-exhibit.jsx
+++ pages/exhibition/why-exhibit.jsx
@@ -5,7 +5,6 @@ import ByExhibit from "components/EvSections/why-exhibit-sections/ByExhibit";
 import ExpectMeet from "components/EvSections/why-exhibit-sections/ExpectMeet";
 import Footer from "components/EvSections/why-exhibit-sections/Footer";
 
-
 import EvLayout from "components/layouts/EvLayout";
 import api from "utils/api/grocery3-shop";
 // ======================================================
@@ -36,7 +35,8 @@ const serviceList = [
   },
   {
     img: "/assets/images/why-exhibit/3.png",
-    content: "Network with global representatives from across the H/EV supply chain",
+    content:
+      "Network with global representatives from across the H/EV supply chain",
   },
   {
     img: "/assets/images/why-exhibit/4.png",
@@ -44,7 +44,8 @@ const serviceList = [
   },
   {
     img: "/assets/images/why-exhibit/5.png",
-    content: "Benchmark your products and services against industry competition",
+    content:
+      "Benchmark your products and services against industry competition",
   },
   {
     img: "/assets/images/why-exhibit/6.png",
@@ -55,69 +56,84 @@ const serviceList = [
 const featureList = [
   {
     img: "/assets/images/why-exhibit/7.jpg",
-    content : "You can preview new and improved solutions for your applications during the new product showcase. Attend live demonstrations at the booths to learn about new and improved products.",
+    content:
+      "You can preview new and improved solutions for your applications during the new product showcase. Attend live demonstrations at the booths to learn about new and improved products.",
     buttonText: "DOWNLOAD BROCHURE NOW",
     buttonLink: "/",
     title: "New Product Showcase",
   },
   {
     img: "/assets/images/why-exhibit/8.jpg",
-    content : "Explore the latest technical content and developments in the industry which includes presentations, panel discussions and case studies, all in the show floor and free to attend for everyone.",
+    content:
+      "Explore the latest technical content and developments in the industry which includes presentations, panel discussions and case studies, all in the show floor and free to attend for everyone.",
     buttonText: "BOOK A STAND",
     buttonLink: "/",
     title: "Open Tech Sessions",
-  }
+  },
 ];
 
 const footer1 = [
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQyODYxZjIzZDk0NjRmNjA=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQyODYxZjIzZDk0NjRmNjA=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ0ODYxZjIzZGE4N2NhZWM=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ0ODYxZjIzZGE4N2NhZWM=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ3MzYxZjIzZGMxMDU2Mjc=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ3MzYxZjIzZGMxMDU2Mjc=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ4NTYxZjIzZGNkZTU0ODc=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0MzI2NTQ4NTYxZjIzZGNkZTU0ODc=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Njk4NTkyODYyMmIwMmM4OTk4YjE=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Njk4NTkyODYyMmIwMmM4OTk4YjE=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Njk4NTk2MTYyMmIwMmU5OGQ3MjA=.png",
-  }
-]
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Njk4NTk2MTYyMmIwMmU5OGQ3MjA=.png",
+  },
+];
 
 const footer2 = [
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjE0MjYxZmQyZDNlNGE3OWY=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjE0MjYxZmQyZDNlNGE3OWY=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjEzMjYxZmQyZDM0NWQ4MzE=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjEzMjYxZmQyZDM0NWQ4MzE=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjExMDYxZmQyZDFlNGVlMjY=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjExMDYxZmQyZDFlNGVlMjY=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjEyMzYxZmQyZDJiMGY1NTE=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0Mzk4MjEyMzYxZmQyZDJiMGY1NTE=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0NjAyNjk5NTYyMWM2MGYzZTVhODE=.png",
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0NjAyNjk5NTYyMWM2MGYzZTVhODE=.png",
   },
   {
-      source: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0NjAyNjk4NDYyMWM2MGU4ZGVhYjI=.png",
-  }
-]
-
+    source:
+      "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/MTY0NjAyNjk4NDYyMWM2MGU4ZGVhYjI=.png",
+  },
+];
 
 const whyExhibit = {
   img: "/assets/images/why-exhibit/charger.png",
   title: " ",
-  p1: `EV innovation is accelerating due to contributions from all around the globe. Improvement in batteries, motor control, and support software will continue to occur and will make EVs a better investment.`, 
-  p2 :`The exhibition will provide a world-class environment for EV industry to showcase the latest solutions in front of an audience who matters, including financiers and investors, engineers, R&D & government officials.`,
-  p3 :`More than 5,000 professionals representing leading companies in the EV industry will be in Abu Dhabi with the goal of networking and sourcing the latest technologies across the 3 days of exhibition.`,
+  ps: [
+    `EV innovation is accelerating due to contributions from all around the globe. Improvement in batteries, motor control, and support software will continue to occur and will make EVs a better investment.`,
+    `The exhibition will provide a world-class environment for EV industry to showcase the latest solutions in front of an audience who matters, including financiers and investors, engineers, R&D & government officials.`,
+    `More than 5,000 professionals representing leading companies in the EV industry will be in Abu Dhabi with the goal of networking and sourcing the latest technologies across the 3 days of exhibition.`,
+  ],
   direction: "right",
 };
 
@@ -129,15 +145,12 @@ const generalPage = () => {
           mb: 6,
         }}
       >
-        <WhyExhibit whyExhibit = {whyExhibit} />
-        <ExhibitionFeatures data = {featureList} />
-        <ByExhibit data = {serviceList} />
-      <ExpectMeet data = {peopleList}/>
-      <Footer footer1 = {footer1} footer2 = {footer2}/>
-
+        <WhyExhibit whyExhibit={whyExhibit} />
+        <ExhibitionFeatures data={featureList} />
+        <ByExhibit data={serviceList} />
+        <ExpectMeet data={peopleList} />
+        <Footer footer1={footer1} footer2={footer2} />
       </Container>
-
-
     </EvLayout>
   );
 };
