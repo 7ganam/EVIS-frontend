f95d8f0ff882822488953c99e3f8735ad67f9699
tarekanwer <tito.a7mad@gmail.com>
2022-08-11 19:44:50 +0200

    quad-img

--- pages/exhibition/about.jsx
+++ pages/exhibition/about.jsx
@@ -9,9 +9,13 @@ import ExpectMeet from "components/EvSections/about-page-sections/ExpectMeet";
 import Facts from "components/EvSections/about-page-sections/Facts";
 import Footer from "components/EvSections/why-exhibit-sections/Footer";
 import {H3} from "components/EvComponents/Typography";
+import QuadImg from 'components/EvComponents/QuadImg';
+
 
 // ======================================================
 // ======================================================
+
+
 
 const section = {
   p1: "As is true for many emerging technologies, vehicle electrification is experiencing rapid innovation. The Middle East & Africa Electric Vehicle Market is expected to witness substantial growth & business opportunities over the next decade. Governments are focusing on renewable energy and clean transportation technologies along with the implementation of economic and energy diversification plans.",
@@ -88,73 +92,74 @@ const item = {
 
 const itemsData = [
   {
-      img : "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/1200x0/MTYzMTc5NzcxODYxNDM0MWQ2M2VhYWQ=.jpeg",
-      title : "New Energy Vehicles",
-      ps : [
-           "Electric Passenger Vehicles",
-           "Electric Light Vehicles",
-           "Electric Commercial Vehicles",
-           "Electric Motorcycles",
-           "Electric Scooters",
-           "Electric Bicycles",
-           "Hybrid Electric Vehicles",
-           "Fuel Cell Electric Vehicles"
-          ],
-      direction : "left"
+    img: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/1200x0/MTYzMTc5NzcxODYxNDM0MWQ2M2VhYWQ=.jpeg",
+    title: "New Energy Vehicles",
+    ps: [
+      "Electric Passenger Vehicles",
+      "Electric Light Vehicles",
+      "Electric Commercial Vehicles",
+      "Electric Motorcycles",
+      "Electric Scooters",
+      "Electric Bicycles",
+      "Hybrid Electric Vehicles",
+      "Fuel Cell Electric Vehicles",
+    ],
+    direction: "left",
   },
   {
-      img : "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/1200x0/MTYzMTc5Nzc1NDYxNDM0MWZhZGY1NTU=.jpeg",
-      title : "Energy & Infrastructure",
-      ps : [
-          "Electric Energy Suppliers",
-          "Hydrogen Energy Suppliers",
-          "Energy Infrastructure",
-          "Intelligent Grid V2G",
-          "Electrical Cables",
-          "Connectors",
-          "Fast Charging Stations",
-          "Energy and Charging Systems",
-          "Charging Infrastructure",
-          "Hydrogen Fuel infrastructure",
-          "Communication Infrastructure",
-      ],
-      direction : "right"
+    img: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/1200x0/MTYzMTc5Nzc1NDYxNDM0MWZhZGY1NTU=.jpeg",
+    title: "Energy & Infrastructure",
+    ps: [
+      "Electric Energy Suppliers",
+      "Hydrogen Energy Suppliers",
+      "Energy Infrastructure",
+      "Intelligent Grid V2G",
+      "Electrical Cables",
+      "Connectors",
+      "Fast Charging Stations",
+      "Energy and Charging Systems",
+      "Charging Infrastructure",
+      "Hydrogen Fuel infrastructure",
+      "Communication Infrastructure",
+    ],
+    direction: "right",
   },
   {
-      img : "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/1200x0/MTYzMTc5Nzc5NzYxNDM0MjI1YmJhMGU=.jpeg",
-      title : "Batteries",
-      ps : [
-          "Lithium Batteries, Lead-acid Batteries, Nickel",
-          "Batteries(MH-Ni Battery) & Other Batteries",
-          "Battery Management",
-          "Battery Charging Systems",
-          "Testing Systems & Testing Equipment",
-          "Capacitors",
-          "Fuel Cell Technology, Fuel Cell Systems & Fuel Cell",
-          "Management",
-          "Hydrogen Tank & Hydrogen Refueling",
-          "Battery Manufacturing Equipment",
-          "Raw Materials",
-          "Parts & Components"
-      ],
-      direction : "left"
+    img: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/1200x0/MTYzMTc5Nzc5NzYxNDM0MjI1YmJhMGU=.jpeg",
+    title: "Batteries",
+    ps: [
+      "Lithium Batteries, Lead-acid Batteries, Nickel",
+      "Batteries(MH-Ni Battery) & Other Batteries",
+      "Battery Management",
+      "Battery Charging Systems",
+      "Testing Systems & Testing Equipment",
+      "Capacitors",
+      "Fuel Cell Technology, Fuel Cell Systems & Fuel Cell",
+      "Management",
+      "Hydrogen Tank & Hydrogen Refueling",
+      "Battery Manufacturing Equipment",
+      "Raw Materials",
+      "Parts & Components",
+    ],
+    direction: "left",
   },
   {
-      img : "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/1200x0/MTYzMTc5Nzg2NTYxNDM0MjY5NmU3YzI=.jpeg",
-      title : "Powertrain",
-      ps : [
-          "General Motors",
-          "In-wheel Motor",
-          "Hybrid Engines",
-          "Cable Braider and Automotive Wiring",
-          "Drive Systems",
-          "Transmission",
-          "Braking Technology and Components",
-          "Other Powertrain Components"
-      ],
-      direction : "right"
-  }
+    img: "https://di9mr54a05a64.cloudfront.net/api-ntravel.expoplatform.com/image/1200x0/MTYzMTc5Nzg2NTYxNDM0MjY5NmU3YzI=.jpeg",
+    title: "Powertrain",
+    ps: [
+      "General Motors",
+      "In-wheel Motor",
+      "Hybrid Engines",
+      "Cable Braider and Automotive Wiring",
+      "Drive Systems",
+      "Transmission",
+      "Braking Technology and Components",
+      "Other Powertrain Components",
+    ],
+    direction: "right",
+  },
 ];
+
 const features = [
   "Recent announcements by vehicle manufacturers intending to electrify the car and buses markets.",
   "Battery manufacturing is undergoing important transitions, including major investments to expand production.",
@@ -203,6 +208,7 @@ const generalPage = () => {
         <ExpectMeet data = {features} />
         </Container>
         <Facts data={facts} p={p}/>
+        <QuadImg data = {itemsData} />
         <Container sx={{ mb: 6 }}>
         <Footer footer1={footer1} footer2={footer2} />
       </Container>
