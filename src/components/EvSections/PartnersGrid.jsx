cc71a4e68b36ef615aab9881f23b1a7be3a78f30
tarekanwer <tito.a7mad@gmail.com>
2022-09-05 02:20:52 +0200

    separate text from the image

--- src/components/EvSections/PartnersGrid.jsx
+++ src/components/EvSections/PartnersGrid.jsx
@@ -1,49 +1,59 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
+import { H5 } from "components/EvComponents/Typography";
+
 
 const StyledImage = (props) => {
-    return (
-        <img src={props.Src} width="170" style={{ margin: "10px" }} />
-    )
-}
+  return <img src={props.Src} width="170" style={{ marginBottom: "10px" }} />;
+};
 
 const imagesData = [
-    {
-        source: "/assets/images/partners/CEBC.png"
-    },
-    {
-        source: "/assets/images/partners/CHARIN.png"
-    },
-    {
-        source: "/assets/images/partners/GEEE.png"
-    },
-    {
-        source: "/assets/images/partners/ISF.png"
-    },
-    {
-        source: "/assets/images/partners/AVERE.png"
-    },
-    {
-        source: "/assets/images/partners/Oxford.png"
-    },
+  {
+    source: "/assets/images/partners/CEBC.png",
+    text: "Sustainability Partner",
+  },
+  {
+    source: "/assets/images/partners/CHARIN.png",
+    text: "Venue Partner",
+  },
+  {
+    source: "/assets/images/partners/GEEE.png",
+    text: "Host City",
+  },
+  {
+    source: "/assets/images/partners/ISF.png",
+    text: "Organized By",
+  },
+  {
+    source: "/assets/images/partners/AVERE.png",
+    text: "Gold Sponsor",
+  },
+  {
+    source: "/assets/images/partners/Oxford.png",
+    text: "Silver Sponsor",
+  },
 ];
 
 const PartnersGrid = () => {
-    return (
-        <Box sx={{ textAlign: "center", paddingBottom: "3rem" }}>
-            {/* <h6>KNOWLEDGE PARTNERS</h6>, */}
-            <Typography variant="h5" component="h5">KNOWLEDGE PARTNERS</Typography>
-            <Grid container
-                sx={{ textAlign: "center", placeItems: "center", }}
-            >
-                {imagesData.map(({ source }) => {
-                    return (
-                        <Grid item xs={12} sm={4} md={2} key={source}>
-                            <StyledImage Src={source} />
-                        </Grid>)
-                })}
+  return (
+    <Box sx={{ textAlign: "center", paddingBottom: "3rem" }}>
+      {/* <h6>KNOWLEDGE PARTNERS</h6>, */}
+      <Typography variant="h5" component="h5">
+        KNOWLEDGE PARTNERS
+      </Typography>
+      <Grid container sx={{ textAlign: "center", placeItems: "center" }} mt = {3}>
+        {imagesData.map(({ source, text }) => {
+          return (
+            <Grid item xs={12} sm={4} md={2} key={source}>
+              <Box>
+                <H5>{text}</H5>
+              </Box>
+              <StyledImage Src={source} />
             </Grid>
-        </Box>
-    )
-}
+          );
+        })}
+      </Grid>
+    </Box>
+  );
+};
 
 export default PartnersGrid;
--- src/components/EvSections/SponsorsGrid.jsx
+++ src/components/EvSections/SponsorsGrid.jsx
@@ -1,47 +1,53 @@
 import { Box, Button, Grid, styled, useTheme } from "@mui/material";
+import { H5 } from "components/EvComponents/Typography";
+
 
 const StyledImage = (props) => {
-    return (
-
-        <img src={props.Src} width="170" style={{ margin: "10px" }} />
-
-    )
-}
+  return <img src={props.Src} width="170" style={{ margin: "10px" }} />;
+};
 
 const imagesData = [
-    {
-        source: "/assets/images/organizers/Masdar.png"
-    },
-    {
-        source: "/assets/images/organizers/ADNEC.png"
-    },
-    {
-        source: "/assets/images/organizers/AbuDhabi.png"
-    },
-    {
-        source: "/assets/images/organizers/Nirvana.png"
-    },
-    {
-        source: "/assets/images/organizers/Audi.png"
-    },
-    {
-        source: "/assets/images/organizers/BritishVolt.png"
-    },
+  {
+    source: "/assets/images/organizers/Masdar.png",
+    text: "Sustainability Partner",
+  },
+  {
+    source: "/assets/images/organizers/ADNEC.png",
+    text: "Venue Partner",
+  },
+  {
+    source: "/assets/images/organizers/AbuDhabi.png",
+    text: "Host City",
+  },
+  {
+    source: "/assets/images/organizers/Nirvana.png",
+    text: "Organized By",
+  },
+  {
+    source: "/assets/images/organizers/Audi.png",
+    text: "Gold Sponsor",
+  },
+  {
+    source: "/assets/images/organizers/BritishVolt.png",
+    text: "Silver Sponsor",
+  },
 ];
 
 const SponsorsGrid = () => {
-    return (
-        <Grid container
-            sx={{ textAlign: "center", placeItems: "center", }}
-        >
-            {imagesData.map(({ source }) => {
-                return (
-                    <Grid item xs={12} sm={4} md={2} key={source}>
-                        <StyledImage Src={source} />
-                    </Grid>)
-            })}
-        </Grid>
-    )
-}
+  return (
+    <Grid container sx={{ textAlign: "center", placeItems: "center" }}>
+      {imagesData.map(({ source, text }) => {
+        return (
+          <Grid item xs={12} sm={4} md={2} key={source}>
+            <Box>
+              <H5>{text}</H5>
+            </Box>
+            <StyledImage Src={source} />
+          </Grid>
+        );
+      })}
+    </Grid>
+  );
+};
 
 export default SponsorsGrid;
