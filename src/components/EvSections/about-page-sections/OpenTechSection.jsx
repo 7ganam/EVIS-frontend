e0422f89609929f6e37f78f6c933168620a7009d
7ganam@gmail.com <7ganam@gmail.com>
2022-09-13 11:38:57 +0200

    updated open tech vids

--- src/components/EvSections/about-page-sections/OpenTechSection.jsx
+++ src/components/EvSections/about-page-sections/OpenTechSection.jsx
@@ -4,7 +4,33 @@ import { Grid, Box, useTheme } from "@mui/material";
 import { SectionTitle } from "src/components/EvComponents/StyledTypography";
 import YoutubeEmbed from "src/components/YoutubeEmbed";
 import { Paragraph } from "src/components/EvComponents/Typography";
-
+import Videos from "src/components/EvSections/new-page-sections/Videos";
+const videosList = [
+  {
+    youtube:
+      "https://www.youtube.com/watch?v=BpyJxxkFytY&list=PLZmjl1dQxsOVKeNWFUIwkIGoSOfE4uPQb&index=1&ab_channel=EVIS",
+  },
+  {
+    youtube:
+      "https://www.youtube.com/watch?v=PrS08I6_1tc&list=PLZmjl1dQxsOVKeNWFUIwkIGoSOfE4uPQb&index=3&ab_channel=EVIS",
+  },
+  {
+    youtube:
+      "https://www.youtube.com/watch?v=cAVSfB97FwI&list=PLZmjl1dQxsOVKeNWFUIwkIGoSOfE4uPQb&index=3&ab_channel=EVIS",
+  },
+  {
+    youtube:
+      "https://www.youtube.com/watch?v=tj2qVkh42R8&list=PLZmjl1dQxsOVKeNWFUIwkIGoSOfE4uPQb&index=4&ab_channel=EVIS",
+  },
+  {
+    youtube:
+      "Navigating EV Charging Infrastructure ;Providing Access to EV Charging from Homes to Highways",
+  },
+  {
+    youtube:
+      "https://www.youtube.com/watch?v=ZBl00Gja9Ow&list=PLZmjl1dQxsOVKeNWFUIwkIGoSOfE4uPQb&index=7&ab_channel=EVIS",
+  },
+];
 const OpenTechSection = (props) => {
   const theme = useTheme();
   const item = props.item;
@@ -20,29 +46,7 @@ const OpenTechSection = (props) => {
         ></TwoColumnBoxV2>
       </Box>
       <Box mt={5}>
-        <Grid container mb={-0.5} spacing={3}>
-          {props.item.videosList.map((item, ind) => (
-            <Grid key={ind} item md={4} sm={6} xs={12}>
-              <Box
-                sx={{
-                  p: "5px",
-                  border: `solid 1px ${theme.palette.primary.main}`,
-                  borderRadius: "10px",
-                }}
-              >
-                <Box
-                  hoverEffect
-                  sx={{
-                    borderRadius: "10px",
-                    overflow: "hidden",
-                  }}
-                >
-                  <YoutubeEmbed embedId={item.youtube} />
-                </Box>
-              </Box>
-            </Grid>
-          ))}
-        </Grid>
+        <Videos videosList={videosList} />
       </Box>
     </Box>
   );
