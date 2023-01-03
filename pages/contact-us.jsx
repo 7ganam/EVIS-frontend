1a2264ff1715c4afbf3e76f56853dec070c0c65a
ganam <7ganam@gmail.com>
2023-01-03 06:05:32 +0200

    finished news and contacts

--- pages/contact-us.jsx
+++ pages/contact-us.jsx
@@ -12,29 +12,60 @@ import { useMemo } from "react";
 // ======================================================
 
 const EvContactUs = (props) => {
-
-  let {
-    contactPageData,
-    InnovationTeamData,
-    titleOfInnovationTeam
-  } = useMemo(() => {
-    if (!props?.contactPage) {
+  let sponsors = useMemo(() => {
+    if (!props?.sponsors) {
       return {};
     }
-    let contactPageData = JSON.parse(props.contactPage).data?.attributes ?? null;
-    const titleOfInnovationTeam = contactPageData?.title_of_innovation_team;
-    if (!props?.InnovationTeam) {
-      return {};
-    }
-    let InnovationTeamData = props.InnovationTeam;
 
-    return {
-      contactPageData,
-      InnovationTeamData,
-      titleOfInnovationTeam,
+    let data = JSON.parse(props.sponsors)?.data ?? null;
 
-    };
-  }, [props?.contactPage, props?.InnovationTeam]);
+    const sponsors = data?.map((sponsor) => {
+      return {
+        text: sponsor?.attributes?.title ?? "",
+        source: sponsor?.attributes?.image?.data?.attributes?.url ?? "",
+        year: sponsor?.attributes?.year ?? "",
+        key_partner: sponsor?.attributes?.key_partner ?? null,
+        sponsor: sponsor?.attributes?.sponsor ?? null,
+        international_media_partner:
+          sponsor?.attributes?.international_media_partner ?? null,
+        knowledge_partner: sponsor?.attributes?.knowledge_partner ?? null,
+        research_partner: sponsor?.attributes?.research_partner ?? null,
+        media_partner: sponsor?.attributes?.media_partner ?? null,
+        link: sponsor?.attributes?.link ?? null,
+      };
+    });
+
+    return sponsors;
+  }, [props?.sponsors]);
+  const key_partners = sponsors?.filter((sponsor) => {
+    return sponsor.key_partner === true;
+  });
+  const knowledgePartners = sponsors?.filter((partner) => {
+    return partner.knowledge_partner === true;
+  });
+  const mediaPartners = sponsors?.filter((partner) => {
+    return partner.media_partner === true;
+  });
+
+  let { contactPageData, InnovationTeamData, titleOfInnovationTeam } =
+    useMemo(() => {
+      if (!props?.contactPage) {
+        return {};
+      }
+      let contactPageData =
+        JSON.parse(props.contactPage).data?.attributes ?? null;
+      const titleOfInnovationTeam = contactPageData?.title_of_innovation_team;
+      if (!props?.InnovationTeam) {
+        return {};
+      }
+      let InnovationTeamData = props.InnovationTeam;
+
+      return {
+        contactPageData,
+        InnovationTeamData,
+        titleOfInnovationTeam,
+      };
+    }, [props?.contactPage, props?.InnovationTeam]);
 
   return (
     <EvLayout showNavbar={true} title={"Contact-Us"}>
@@ -44,13 +75,29 @@ const EvContactUs = (props) => {
         }}
       >
         <FindUsSection contactPage={contactPageData} />
-        <InnovationTeamSection InnovationTeam={InnovationTeamData} Title={titleOfInnovationTeam} />
+        <InnovationTeamSection
+          InnovationTeam={InnovationTeamData}
+          Title={titleOfInnovationTeam}
+        />
       </Container>
       <AboutNirvana contactPage={contactPageData} />
       <Container>
         <Box sx={{ mt: "100px" }}>
-          <SponsorsGrid />
-          <PartnersGrid sx={{ mt: 5 }} />
+          <PartnersGrid
+            sx={{ mt: 5 }}
+            partners={key_partners}
+            title={"KEY PARTNERS"}
+          />
+          <PartnersGrid
+            sx={{ mt: "80px" }}
+            partners={knowledgePartners}
+            title={"KNOWLEDGE PARTNERS"}
+          />
+          <PartnersGrid
+            sx={{ mt: "80px", pb: "100px" }}
+            partners={mediaPartners}
+            title={"MEDIA PARTNERS"}
+          />
         </Box>
       </Container>
     </EvLayout>
@@ -84,8 +131,24 @@ export async function getStaticProps(context) {
       notFound: true,
     };
   }
+
+  let sponsors = null;
+  let sponsorsError = null;
+  try {
+    sponsors = await api.getSponsors(23);
+  } catch (dev_error) {
+    console.log(`error fetching`, dev_error);
+    sponsorsError = dev_error;
+  }
+  if (!sponsors) {
+    return {
+      notFound: true,
+    };
+  }
   return {
     props: {
+      sponsors: JSON.stringify(sponsors),
+      sponsorsError: JSON.stringify(sponsorsError),
       contactPage: JSON.stringify(contactPage),
       contactPageError: JSON.stringify(contactPageError),
       InnovationTeam: JSON.stringify(InnovationTeam),
--- pages/news-media/news.jsx
+++ pages/news-media/news.jsx
@@ -15,6 +15,42 @@ import { useMemo } from "react";
 
 const EvHome = (props) => {
   const theme = useTheme();
+
+  let sponsors = useMemo(() => {
+    if (!props?.sponsors) {
+      return {};
+    }
+
+    let data = JSON.parse(props.sponsors)?.data ?? null;
+
+    const sponsors = data?.map((sponsor) => {
+      return {
+        text: sponsor?.attributes?.title ?? "",
+        source: sponsor?.attributes?.image?.data?.attributes?.url ?? "",
+        year: sponsor?.attributes?.year ?? "",
+        key_partner: sponsor?.attributes?.key_partner ?? null,
+        sponsor: sponsor?.attributes?.sponsor ?? null,
+        international_media_partner:
+          sponsor?.attributes?.international_media_partner ?? null,
+        knowledge_partner: sponsor?.attributes?.knowledge_partner ?? null,
+        research_partner: sponsor?.attributes?.research_partner ?? null,
+        media_partner: sponsor?.attributes?.media_partner ?? null,
+        link: sponsor?.attributes?.link ?? null,
+      };
+    });
+
+    return sponsors;
+  }, [props?.sponsors]);
+  const key_partners = sponsors?.filter((sponsor) => {
+    return sponsor.key_partner === true;
+  });
+  const knowledgePartners = sponsors?.filter((partner) => {
+    return partner.knowledge_partner === true;
+  });
+  const mediaPartners = sponsors?.filter((partner) => {
+    return partner.media_partner === true;
+  });
+
   let { evisNewsData, evisInNewsData } = useMemo(() => {
     if (!props?.evisNews) {
       return {};
@@ -146,8 +182,23 @@ const EvHome = (props) => {
         <Press data={news} />
         <Press2 data={news2} /> */}
         {/* <Footer footer1={footer1} footer2={footer2} /> */}
-        <SponsorsGrid />
-        <PartnersGrid />
+        <Box sx={{ mt: "100px" }}>
+          <PartnersGrid
+            sx={{ mt: 5 }}
+            partners={key_partners}
+            title={"KEY PARTNERS"}
+          />
+          <PartnersGrid
+            sx={{ mt: "80px" }}
+            partners={knowledgePartners}
+            title={"KNOWLEDGE PARTNERS"}
+          />
+          <PartnersGrid
+            sx={{ mt: "80px", pb: "100px" }}
+            partners={mediaPartners}
+            title={"MEDIA PARTNERS"}
+          />
+        </Box>
       </Container>
     </EvLayout>
   );
@@ -185,8 +236,24 @@ export async function getStaticProps(context) {
     };
   }
 
+  let sponsors = null;
+  let sponsorsError = null;
+  try {
+    sponsors = await api.getSponsors(23);
+  } catch (dev_error) {
+    console.log(`error fetching`, dev_error);
+    sponsorsError = dev_error;
+  }
+  if (!sponsors) {
+    return {
+      notFound: true,
+    };
+  }
+
   return {
     props: {
+      sponsors: JSON.stringify(sponsors),
+      sponsorsError: JSON.stringify(sponsorsError),
       evisNews: JSON.stringify(evisNews),
       evisNewsError: JSON.stringify(evisNewsError),
       newsArticle: JSON.stringify(newsArticle),
